904b5e5c40d9a766f638ae8fb3b852e83aecdb21
Minh Pham <phamtuannhatminh@gmail.com>
2023-06-19 10:45:48 +0700

    Initialize manifest.json and interface, error with openai API key

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+/*global chrome*/
+
+// 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
+import React, {useState} from "react"
 import './App.css';
+import {Box, Button, Paper, Container, Grid, TextField} from "@mui/material"
+import AutorenewIcon from "@mui/icons-material/Autorenew"
+import {Configuration, OpenAIApi} from 'openai'
 
 function App() {
+  const [prompt, setPrompt] = useState("")
+  const [response, setResponse] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const configuration = new Configuration({
+    apiKey: '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',
+  })
+  const openai = new OpenAIApi(configuration)
+
+  async function handleSubmit() {
+    setIsLoading(true)
+    // try {
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: prompt,
+      max_tokens: 20
+    })
+    setResponse(completion.data.choices[0].text)
+    setIsLoading(false)
+  //   } catch(e) {
+  //     alert("Error: ", e)
+  //     setIsLoading(false)
+  //   }
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Box sx={{width: '100%', mt: 4}}>
+        <Grid container>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              autoFocus
+              label="Type your question"
+              variant="outlined"
+              multiline
+              rows={4}
+              margin='normal'
+              value={prompt}
+              onChange={(e) => {
+                setPrompt(e.target.value)
+              }}
+            />
+            <Button 
+              fullWidth
+              disableElevation
+              disabled={isLoading}
+              variant='contained'
+              onClick={() => handleSubmit()}
+              startIcon={
+                isLoading && (
+                  <AutorenewIcon
+                    sx={{
+                      animation: 'spin 2s linear infinite',
+                      "@keyframes spin": {
+                        "0%": {
+                          transform: 'rotate(360deg)',
+                        },
+                        '100%': {
+                          transform: 'rotate(0deg)',
+                        },
+                      },
+                    }}
+                  />
+                )
+              }
+            >
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sx={{mt: 3}}>
+          <Paper sx={{p: 3}}>{response}</Paper>
+        </Grid>
+      </Box>
+    </Container>
   );
 }
 
